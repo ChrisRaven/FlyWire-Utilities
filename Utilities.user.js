f7886fb65274471a2cbc47624671dd30d75593e7
KrzysztofKruk <kr.kruk@gmail.com>
2022-06-29 23:00:15 +0200

    Fix update link (again)

--- Utilities.user.js
+++ Utilities.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         Utilities
 // @namespace    KrzysztofKruk-FlyWire
-// @version      0.2
+// @version      0.2.0.1
 // @description  Various functionalities for FlyWire
 // @author       Krzysztof Kruk
 // @match        https://ngl.flywire.ai/*
 // @grant        GM_xmlhttpRequest
 // @grant        unsafeWindow
 // @connect      services.itanna.io
-// @updateURL    https://raw.githubusercontent.com/ChrisRaven/FlyWire-Utilities/main/utilities.user.js
-// @downloadURL  https://raw.githubusercontent.com/ChrisRaven/FlyWire-Utilities/main/utilities.user.js
+// @updateURL    https://raw.githubusercontent.com/ChrisRaven/FlyWire-Utilities/main/Utilities.user.js
+// @downloadURL  https://raw.githubusercontent.com/ChrisRaven/FlyWire-Utilities/main/Utilities.user.js
 // @homepageURL  https://github.com/ChrisRaven/FlyWire-Utilities
 // ==/UserScript==
 
