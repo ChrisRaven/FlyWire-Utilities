49f52dddfec4fb6544f59f5d279c19f6031dbd4d
KrzysztofKruk <kr.kruk@gmail.com>
2022-07-02 23:42:58 +0200

    New way of creating Dock

--- Utilities.user.js
+++ Utilities.user.js
@@ -13,11 +13,14 @@
 // @homepageURL  https://github.com/ChrisRaven/FlyWire-Utilities
 // ==/UserScript==
 
-if (unsafeWindow.dockIsReady) return main()
+// if (unsafeWindow.dockIsReady) return main()
 
-let script = document.createElement('script')
-script.src = typeof DEV !== 'undefined' ? 'http://127.0.0.1:5501/FlyWire-Dock/Dock.js' : 'https://chrisraven.github.io/FlyWire-Dock/Dock.js'
-document.head.appendChild(script)
+if (!document.getElementById('dock-script')) {
+  let script = document.createElement('script')
+  script.id = 'dock-script'
+  script.src = typeof DEV !== 'undefined' ? 'http://127.0.0.1:5501/FlyWire-Dock/Dock.js' : 'https://chrisraven.github.io/FlyWire-Dock/Dock.js'
+  document.head.appendChild(script)
+}
 
 let wait = setInterval(() => {
   if (unsafeWindow.dockIsReady) {
