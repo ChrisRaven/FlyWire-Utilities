c333405dd5ad5104b8f44b22632e86e41d9d3790
KrzysztofKruk <kr.kruk@gmail.com>
2023-02-10 21:51:13 +0100

    Update for new FW url

--- Utilities.user.js
+++ Utilities.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Utilities
 // @namespace    KrzysztofKruk-FlyWire
-// @version      0.20.1
+// @version      0.20.2
 // @description  Various functionalities for FlyWire
 // @author       Krzysztof Kruk
 // @match        https://ngl.flywire.ai/*
-// @match        https://proofreading.flywire.ai/*
+// @match        https://edit.flywire.ai/*
 // @grant        GM_xmlhttpRequest
 // @grant        unsafeWindow
 // @connect      services.itanna.io
